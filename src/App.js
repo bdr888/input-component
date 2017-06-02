4a892a531d9473ac7175ea5c3369e63024005d83
briandridge <briandridge@gmail.com>
2017-06-02 15:07:58 -0600

    adds input component, nothing working yet

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  renderField(field) {
+    return(
+      <div>
+        <div>
+          <label>Your Email Address</label>
+          <input
+            className="form-control"
+            type="text"
+            // {...field.input}
+          />
+        </div>
+
+        <div>
+          <label>Enter a Password  (4-30 characters)</label>
+          <input
+            className="form-control"
+            type="text"
+            // {...field.input}
+          />
+        </div>
+      </div>
+    );
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Input component</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
